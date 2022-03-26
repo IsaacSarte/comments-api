e4b564b35f7e0c8f8ef483c80cb735a6cc76e931
Isaac Sarte <aizakku09@gmail.com>
2022-03-26 17:46:23 +0800

    added devise and jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  },
+  defaults: {format: :json}
+
+
   namespace :api do
     namespace :v1 do
       resources :comments
